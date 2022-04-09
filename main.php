e971903e7028387ca6493dde48437e8ee89c6b6d
itona-22FH <naov7700@gmail.com>
2022-04-09 16:06:53 +0900

    PHP課題1-1

---
+++ main.php
@@ -0,0 +1,33 @@
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>日本の首都は？</h1>
+    <form action="main.php" method="post">
+    <input type="text" name="inputCapital">
+    <input type="submit" value="OK" name="Submit">
+    </form>
+    <?php 
+    
+$jpCapital = '東京';
+
+if(isset($_POST['Submit'])) {
+
+   $inputCapital = $_POST['inputCapital'];
+
+    if($inputCapital === $jpCapital) {
+        echo '正解';
+    } else {
+        echo '不正解';
+    }
+}
+    ?>
+</body>
+</html>
