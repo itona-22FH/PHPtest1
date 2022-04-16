95200c483e97b5641db2f3f7684134f69a20ca0a
itona-22FH <naov7700@gmail.com>
2022-04-16 14:30:54 +0900

    PHP課題1-1 修正

--- main.php
+++ main.php
@@ -1,33 +1,27 @@
-
-
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>日本の首都は？</h1>
-    <form action="main.php" method="post">
-    <input type="text" name="inputCapital">
-    <input type="submit" value="OK" name="submit">
-    </form>
-    <?php 
-
-$jpCapital = '東京';
-
-if(isset($_POST['submit'])) {
-
-   $inputCapital = $_POST['inputCapital'];
-
-    if($inputCapital === $jpCapital) {
-        echo '正解';
-    } else {
-        echo '不正解';
-    }
-}
-    ?>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <h1>日本の首都は？</h1>
+        <form method="post">
+            <input type="text" name="inputCapital">
+            <input type="submit" value="OK" name="submit">
+        </form>
+        <?php
+            $jpCapital = "東京";
+            if(isset($_POST["submit"])) {
+                $inputCapital = $_POST["inputCapital"];
+                if($inputCapital === $jpCapital) {
+                    echo "正解";
+                } else {
+                    echo "不正解";
+                }
+            }
+        ?>
+    </body>
 </html>
