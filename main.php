11fa652a742076f6a03b147b3cfac0e63322f818
itona-22FH <naov7700@gmail.com>
2022-04-29 11:34:25 +0700

    PHP課題１−４(じゃんけん)

--- main.php
+++ main.php
@@ -7,21 +7,64 @@
         <title>Document</title>
     </head>
     <body>
+        <form method="post">
+            <select name="player">
+                <option value="グー">グー</option>
+                <option value="チョキ">チョキ</option>
+                <option value="パー">パー</option>
+            </select>
+            <br>
+            <input type="submit" value="じゃんけん！" name="submit">
+        </form>
         <?php
-            $question["問題"] = "日本の首都は？";
-            $answer["回答1"] = "大阪";
-            $answer["回答2"] = "北海道";
-            $answer["回答3"] = "箱根";
-            $answer["回答4"] = "東京";
+        $comArray = ["グー", "チョキ", "パー"];
 
-            foreach ($question as $questionKey => $questionValue) {
-                echo $questionKey . " " . $questionValue;
+        if (isset($_POST["submit"])) {
+            $player = $_POST['player'];
+            $key = array_rand($comArray);
+            $com = $comArray[$key];
+            if ($player === "グー") {
+                switch ($com) {
+                    case "チョキ":
+                    $result = "あなたの勝利です！";
+                    break;
+                    case "グー":
+                    $result = "あいこ";
+                    break;
+                    case "パー":
+                    $result = "あなたの敗北です。。。";
+                    break;
+                }
+            } elseif ($player === "チョキ") {
+                switch ($com) {
+                    case "パー":
+                    $result = "あなたの勝利です！";
+                    break;
+                    case "チョキ":
+                    $result = "あいこ";
+                    break;
+                    case "グー":
+                    $result = "あなたの敗北です。。。";
+                    break;
+                }
+            } elseif ($player === "パー") {
+                switch ($com) {
+                    case "グー":
+                    $result = "あなたの勝利です！";
+                    break;
+                    case "パー":
+                    $result = "あいこ";
+                    break;
+                    case "チョキ":
+                    $result = "あなたの敗北です。。。";
+                    break;
+                }
             }
-
+            echo "自分: $player";
             echo "<br>";
-
-            foreach ($answer as $answerKey => $answerValue) {
-                echo "<br>" . $answerKey . " " . $answerValue;
+            echo "相手: $com";
+            echo "<br>";
+            echo $result;
             }
         ?>
     </body>
